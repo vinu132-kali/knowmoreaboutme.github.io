6a5de8cadc4ba72b36f520dcf0be51f3df396e7c
vinu132-kali <69195030+vinu132-kali@users.noreply.github.com>
2022-07-11 22:47:21 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+$a=$_POST['username'];
+$b=$_POST['pwd'];
+
+if($a=='Admin' and $b=='admin@123'){
+    header('location: https://vinu132-kali.github.io/knowmoreaboutme.github.io/welcome.html');
+}
+else{
+    echo 'Error :(';
+}
+
+?>
