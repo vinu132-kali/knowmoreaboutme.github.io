d4dd974dd07ae53c4cdbd87138e824a2faec5d0b
vinu132-kali <69195030+vinu132-kali@users.noreply.github.com>
2022-07-14 13:32:59 +0530

    Update login.php
    Signed-off-by: vinu132-kali <69195030+vinu132-kali@users.noreply.github.com>

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ $a=$_POST['username'];
 $b=$_POST['pwd'];
 
 if($a=='Admin' and $b=='admin@123'){
-    header("location:https://www.github.com/vinu132-kali.github.io/knowmoreaboutme.github.io/welcome.html");
+    header("location:https://www.github.com/vinu132-kali.github.io/knowmoreaboutme.github.io/");
 }
 else{
     echo 'Error :(';
